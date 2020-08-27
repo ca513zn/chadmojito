a43b3beae96453ceb5f580be7b211e3300b076bf
czuniga <czuniga@tibco-support.com>
2020-08-27 09:45:06 -0500

    Adding more chad art

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img style={{
-          borderRadius: '100%'
-        }} src={"https://scontent.fmid3-1.fna.fbcdn.net/v/t1.0-9/118507030_1169825093411955_6547663037328192201_o.jpg?_nc_cat=109&_nc_sid=a26aad&_nc_eui2=AeFwvkHJSwUYUjJkqws9ssXozeZykshgfcDN5nKSyGB9wBiY9jxhOfLDefHFo6lf0pzcH_A2F2sF3O3m3ZAhUKyf&_nc_ohc=oGwHyNI6mZcAX8vCsIo&_nc_ht=scontent.fmid3-1.fna&oh=26f4fc7d036e46fe9b5eab4c7371d75b&oe=5F6CA89B"} className="App-logo" alt="logo" />
-        <p>
-          Hey chad! How are you? Just making your art spin.
-        </p>
-        <a
-          className="App-link"
-          href="www.carlosznava.com"
-        >
+        <div
+        style={{
+          display:'flex'
+        }}>
+
+        <img
+          style={{
+            borderRadius: "100%",
+          }}
+          src={
+            "https://scontent.fmid3-1.fna.fbcdn.net/v/t1.0-9/118507030_1169825093411955_6547663037328192201_o.jpg?_nc_cat=109&_nc_sid=a26aad&_nc_eui2=AeFwvkHJSwUYUjJkqws9ssXozeZykshgfcDN5nKSyGB9wBiY9jxhOfLDefHFo6lf0pzcH_A2F2sF3O3m3ZAhUKyf&_nc_ohc=oGwHyNI6mZcAX8vCsIo&_nc_ht=scontent.fmid3-1.fna&oh=26f4fc7d036e46fe9b5eab4c7371d75b&oe=5F6CA89B"
+          }
+          className="App-logo"
+          alt="logo"
+          />
+        <img
+          style={{
+            borderRadius: "100%",
+          }}
+          src={
+            "https://scontent.fmid3-1.fna.fbcdn.net/v/t1.0-9/118467650_1169825136745284_2563399689503029315_o.jpg?_nc_cat=108&_nc_sid=a26aad&_nc_eui2=AeEh2Znyoa4TeMVNhA8OHBBb-BdSRaKJqev4F1JFoomp6wwK-3FtX-ZHm4F8np6EV88uCkgK7QlgWaFSkD_9DEUm&_nc_ohc=1ttPJ10Wd-kAX8DRAAi&_nc_ht=scontent.fmid3-1.fna&oh=bec806695dfd9ee9d98beea9da1642d6&oe=5F6EF097"
+          }
+          className="App-logo"
+          alt="logo"
+          />
+        <img
+          style={{
+            borderRadius: "100%",
+          }}
+          src={
+            "https://scontent.fmid3-1.fna.fbcdn.net/v/t1.0-9/118197622_1169825240078607_4195982046474082933_o.jpg?_nc_cat=108&_nc_sid=a26aad&_nc_eui2=AeHTf0u9r8B-DycdHp_mw5Nw17efpdnVpOXXt5-l2dWk5QFlNvXyWn0P58HhZ9orlp4FfTfymyWAfiLUmibqFeye&_nc_ohc=d_IvWOeItr8AX-q-GuN&_nc_ht=scontent.fmid3-1.fna&oh=51556e1cd895fb5288b7376bb6acb6ba&oe=5F6D463E"
+          }
+          className="App-logo"
+          alt="logo"
+          />
+          </div>
+        <p>Hey chad! How are you? Just making your art spin.</p>
+        <a className="App-link" href="www.carlosznava.com">
           Made by Carlos Z.
         </a>
       </header>
