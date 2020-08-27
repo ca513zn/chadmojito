2f1966920318b76a18387ff245be2d51d7030653
czuniga <czuniga@tibco-support.com>
2020-08-27 10:00:00 -0500

    adding padding to footer and more art

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ function App() {
     "https://scontent.fmid3-1.fna.fbcdn.net/v/t1.0-9/118467650_1169825136745284_2563399689503029315_o.jpg?_nc_cat=108&_nc_sid=a26aad&_nc_eui2=AeEh2Znyoa4TeMVNhA8OHBBb-BdSRaKJqev4F1JFoomp6wwK-3FtX-ZHm4F8np6EV88uCkgK7QlgWaFSkD_9DEUm&_nc_ohc=1ttPJ10Wd-kAX8DRAAi&_nc_ht=scontent.fmid3-1.fna&oh=bec806695dfd9ee9d98beea9da1642d6&oe=5F6EF097",
     "https://scontent.fmid3-1.fna.fbcdn.net/v/t1.0-9/118197622_1169825240078607_4195982046474082933_o.jpg?_nc_cat=108&_nc_sid=a26aad&_nc_eui2=AeHTf0u9r8B-DycdHp_mw5Nw17efpdnVpOXXt5-l2dWk5QFlNvXyWn0P58HhZ9orlp4FfTfymyWAfiLUmibqFeye&_nc_ohc=d_IvWOeItr8AX-q-GuN&_nc_ht=scontent.fmid3-1.fna&oh=51556e1cd895fb5288b7376bb6acb6ba&oe=5F6D463E",
     "https://scontent.fmid3-1.fna.fbcdn.net/v/t1.0-9/118285940_1169825300078601_6176157642665363154_o.jpg?_nc_cat=110&_nc_sid=a26aad&_nc_eui2=AeH8wgMjoV3FFfufVaMoPkBu19SJCiOe_TPX1IkKI579MymMLOFtlHx1T0jA5Mx4RiMxBYglpCwaKNEMMh7Mz93D&_nc_ohc=bguGn-Ocd40AX-LNJKY&_nc_ht=scontent.fmid3-1.fna&oh=97cbb6cfcb3cd6023e1a1580e92b006c&oe=5F6BEBDE",
+    "https://scontent.fmid3-1.fna.fbcdn.net/v/t1.0-9/118499877_1169825410078590_3969398110668329034_o.jpg?_nc_cat=111&_nc_sid=a26aad&_nc_eui2=AeGLiZavq-KQRN0_0_19e6rlaACCg5nqZkRoAIKDmepmRAQfvqSoo3esFdTC6YOcBc0-3YxFU4xg0Ez6XxZaYjpG&_nc_ohc=KuATOmaSleUAX-tr-I3&_nc_ht=scontent.fmid3-1.fna&oh=911e652ba91bdcb2f46423fb123f1d27&oe=5F6C2197",
+    "https://scontent.fmid3-1.fna.fbcdn.net/v/t1.0-9/118353517_1169825466745251_3012706913864521462_o.jpg?_nc_cat=105&_nc_sid=a26aad&_nc_eui2=AeGL4rSfklAYmF-AasNmAhU8JONsHRe-7Lsk42wdF77su5GUlbqtGuq2G5ab0VSTl9tZWWD3M2kIZpMUxxjNWpc2&_nc_ohc=12CYOGejPvIAX-3-KlQ&_nc_ht=scontent.fmid3-1.fna&oh=db1aeae0405b6d6e7f39a8898f5f5a48&oe=5F6E77EE",
+    "https://scontent.fmid3-1.fna.fbcdn.net/v/t1.0-9/118293522_1169825496745248_6078050860291759823_o.jpg?_nc_cat=104&_nc_sid=a26aad&_nc_eui2=AeE2a9NyKJ0kTvWbSbpmtdCSaG1JKonQ2QpobUkqidDZCpNiaERd8_c1dnLt9Inz0R68lifskE9--ISLA3GDVXoD&_nc_ohc=jnNjsFgvpeoAX_SpKP5&_nc_ht=scontent.fmid3-1.fna&oh=48b3ae1ae31afb990979efa2e677f659&oe=5F6E8638",
+    
   ];
   return (
     <div className="App">
       <header className="App-header">
         <div
           style={{
-            marginTop: "16px",
+            padding: "16px",
             display: "flex",
             flexWrap: "wrap",
             justifyContent: "center",
@@ -34,10 +38,20 @@ function App() {
             />
           ))}
         </div>
-        <p>Hey chad! How are you?<br />Just making your art spin.</p>
-        <a className="App-link" href="https://www.carlosznava.com">
-          Made by Carlos Z.
-        </a>
+        <p>
+          Hey chad! How are you?
+          <br />
+          Just making your art spin.
+        </p>
+        <div
+          style={{
+            padding: "16px",
+          }}
+        >
+          <a className="App-link" href="https://www.carlosznava.com">
+            Made by Carlos Z.
+          </a>
+        </div>
       </header>
     </div>
   );
